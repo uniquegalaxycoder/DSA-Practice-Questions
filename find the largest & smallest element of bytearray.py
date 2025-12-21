4709714ff3b5a2635525181a6859f3947e6ef791
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2025-12-21 14:40:53 +0530

    Create find the largest & smallest element of bytearray.py

---
+++ find the largest & smallest element of bytearray.py
@@ -0,0 +1,21 @@
+# find the largest & smallest element of bytearray
+
+arr = [4,5,1,3,2]
+
+n = len(arr)
+
+sml_ele = arr[0]
+max_ele = arr[0]
+
+for i in range(1,n):
+    if arr[i] < sml_ele :
+        sml_ele = arr[i]
+        
+    elif arr[i] > max_ele :
+        max_ele = arr[i]
+        
+print(sml_ele)
+print(max_ele)
+
+# Space complexity O(1) -> only using extra two veriables  
+# time complexity O(n) -> traverse the array once
