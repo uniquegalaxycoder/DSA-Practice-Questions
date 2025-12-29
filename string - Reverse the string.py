294fec327630307de2995fbb1235660c353a8a1d
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2025-12-29 18:11:00 +0530

    Update string - Reverse the string.py

--- string - Reverse the string.py
+++ string - Reverse the string.py
@@ -2,15 +2,32 @@
 Reverse the string
 """
 
-# Approach 1 :  
+# Approach 1 :  string to list & two pointers method
 
 def reverseString(text:str)-> str:
   start = 0 
   end = len(text)-1 
-  n = len(text)//2 
+
   while start <= end :
     text[start], text[end] = text[end], text[start]
     start += 1 
     end -= 1 
-  word = "".join(text)
-  return word 
+  return "".join(text) 
+
+"""
+Time Complexity : O(n)
+Space Complexity : O(n) -> new string reversed string creation
+"""
+
+
+
+# Approach 2 : 
+
+def ReverseStr(text:str):
+  new_list = list(text)
+  text = new_list[::-1]
+  return "".join(text)
+
+text = "Data"
+x = ReverseStr(text)
+print(x)
