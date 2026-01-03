90874661f0cbcdad2b98357984086de3c0bebbd9
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2026-01-03 16:45:28 +0530

    Create array - Max Consicutive one's.py

---
+++ array - Max Consicutive one's.py
@@ -0,0 +1,26 @@
+""" Find the max one's  """
+
+
+def maxOnes(arr:list):
+    n = len(arr)                                      # O(1)
+                                                      
+    count = 0                                         # O(1)
+    max_count = 0                                     # O(1)
+    
+    for i in range(n):                                # O(n)
+        if arr[i] == 1 :                              # O(1)
+            count += 1                                # O(1)
+            max_count = max(max_count, count)         # O(1)
+        else :
+            count = 0                                 # O(1)
+            max_count = max(max_count, count)         # O(1)
+    return max(count, max_count)                      
+    
+arr = [1,1,0,1,1,1,1,0,1]
+print(maxOnes(arr))
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+
+
+    
