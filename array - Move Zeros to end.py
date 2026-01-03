a5eaf6a21781fffefd5b701dd6dc91b3ee17ccf5
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2026-01-03 17:01:39 +0530

    Update array - Move Zeros to end.py

--- array - Move Zeros to end.py
+++ array - Move Zeros to end.py
@@ -6,6 +6,8 @@ Time Complexity : O(n)
 Space Complexity : O(1)
 """
 
+# Better Approach
+
 def MoveZerosEnd(arr:list)-> list :
       n = len(arr)                            # O(1) -> Space Complexity
       j = 0                                   # O(1) -> Time Complexity
@@ -21,3 +23,20 @@ print(x)
 
 # -> [2,1,4,3,0,0,0,0]
 
+# Brute force Solution 
+"""
+Time Complextiy : O(n^2)
+Space COmplexity : O(1)
+"""
+
+def moveZeroEnds(arr:list) :
+      n = len(arr)
+      for i in range(n):
+            for j in range(i, n) :
+                  if arr[i] == 0 :
+                        arr[i], arr[j] = arr[j], arr[i]
+      return arr 
+
+arr = [0,0,2,0,1,0,4,3]
+y = moveZeroEnd(arr)
+print(y)
